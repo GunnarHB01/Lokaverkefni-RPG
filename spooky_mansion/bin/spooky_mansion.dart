3275465b8b70813116652201f285c5fe0d299c90
GunnarHB <98gunnarhelgi@gmail.com>
2024-05-09 18:48:50 +0000

    Skeleton
    Smá prepp og skeleton

---
+++ spooky_mansion/bin/spooky_mansion.dart
@@ -0,0 +1,5 @@
+import 'package:spooky_mansion/main.dart' as spooky_mansion;
+
+void main(List<String> arguments) {
+  print('Hello world: ${spooky_mansion.calculate()}!');
+}
---
+++ spooky_mansion/test/spooky_mansion_test.dart
@@ -0,0 +1,8 @@
+import 'package:spooky_mansion/main.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
